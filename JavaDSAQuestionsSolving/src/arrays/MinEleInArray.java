2e4550c4dd891e31584e65928f1fd10c95077ed4
Komal Narawade <knarawade4@gmail.com>
2025-12-04 12:56:25 +0530

    Day 2

--- JavaDSAQuestionsSolving/src/arrays/MinEleInArray.java
+++ JavaDSAQuestionsSolving/src/arrays/MinEleInArray.java
@@ -12,11 +12,33 @@ public class MinEleInArray {
             	 return min;
              }
              
+             public int secondMinimumElement(int num[]) {
+            	 int m1 = Integer.MAX_VALUE;
+            	 int m2 =Integer.MAX_VALUE;
+            	 
+            	 for(int i=0; i<num.length; i++) {
+            		 if(num[i]<m1) {
+            			 m2 =m1;
+            			 m1 =num[i];
+            			 
+            		 }
+            	 }
+            	 
+            	 
+            	 
+            	 
+            	 
+            	 return m2;
+            	 
+             }
+             
              public static void main(String[] args) {
             	 MinEleInArray m = new MinEleInArray();
             	 int num[] = {32 ,34,56,78,1};
             	 int result = m.minimumElement(num);
+            	 int result1 = m.secondMinimumElement(num);
             	 System.out.println("Minimum Element in array  "+ result);
+            	 System.out.println("Second minimum element in array :" + result1);
 			}
 	
 }
